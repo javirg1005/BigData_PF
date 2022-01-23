b34311e872b8dbdf0b7b0df912a95a9ed7fc5329
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2022-01-23 20:21:28 +0100

    Update streamlit_app.py
    Co-Authored-By: Caco <55552731+cacolyoko@users.noreply.github.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,7 @@ st.vega_lite_chart(data_playercards, {
     },
     'layer': [
         {
-            'mark': {'type': 'bar', 'tooltip': True, 'xOffset': 0, 'size': 10, 'color': '#FB683F'},
+            'mark': {'type': 'bar', 'tooltip': True, 'xOffset': -5, 'size': 10, 'color': '#FB683F'},
             'encoding': {
                 'y': {
                     'field': 'Tarjetas Rojas',
@@ -176,7 +176,7 @@ st.vega_lite_chart(data_playercards, {
             }
         },
         {
-            'mark': {'type': 'bar', 'tooltip': True, 'xOffset': 20, 'size': 10, 'color': '#FCF951'},
+            'mark': {'type': 'bar', 'tooltip': True, 'xOffset': 5, 'size': 10, 'color': '#FCF951'},
             'encoding': {
                 'y': {
                     'field': 'Tarjetas Amarillas',
